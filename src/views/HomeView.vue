8bb8bf3a2a96110c97efd6e4257447a5d980fc72
felpsalvs <profelipealves@outlook.com>
2024-10-01 11:25:18 -0300

    feat: create initial app structure with home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto px-4">
+    <h1 class="text-3xl font-bold mb-4">PokeAPI Vue 3</h1>
+    <pokemon-list />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import PokemonList from "../components/PokemonList.vue";
+</script>
