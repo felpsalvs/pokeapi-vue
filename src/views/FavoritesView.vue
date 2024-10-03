e8a7b3b0a92ab74a8f082e7ae9667e1f84759f3b
felpsalvs <profelipealves@outlook.com>
2024-10-03 18:25:40 -0300

    chore: create FavoritesPage component

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto px-4">
+    <pokemon-list />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import PokemonList from "../components/FavoritesList.vue";
+</script>
